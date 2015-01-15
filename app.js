8dfea60197f9c760fa15ef07d0f31232efa89e70
WitmerDesigns <patrick@witmerdesigns.com>
2015-01-15 12:24:00 -0700

    whoops

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 var main = function() {
 
-<<<<<<< HEAD
 	$('.menu-open').click(function() {
 		$('nav').animate({
 			left: '0px'
@@ -16,31 +15,12 @@ $('.menu-close').click(function() {
 			left: '-285px'
 			}, 200);
 	$('body').animate({
-=======
-	$('.icon-menu').click(function() {
-		$('.menu').animate({
-			left: '0px'
-		}, 200);
-
-		$('body').animate({
-			left: '285px'
-		}, 200);
-	});
-
-	$('.icon-close').click(function() {
-		$('.menu').animate({
-			left: '-285px'
-		}, 200);
-
-		$('body').animate({
->>>>>>> dddda802730c86213eb6ea96fff9cc85b268ef9a
 			left: '0px'
 		}, 200);
 
 	});
 
 
-<<<<<<< HEAD
 
 };
 
@@ -50,8 +30,3 @@ $(document).ready(main);
 
 
 
-=======
-};
-
-$(document).ready(main);
->>>>>>> dddda802730c86213eb6ea96fff9cc85b268ef9a
